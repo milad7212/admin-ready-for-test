11cd59c0b8dad1298e9f98aea222e5ec153143db
miillaadddddd <milad7212@gmail.com>
2023-05-11 22:06:41 +0330

    Add name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import axios from "axios";
 import BookOrdering from "components/ordering/BookOrdering";
 
 import { FiEdit } from "react-icons/fi";
+import { AiFillHeart } from "react-icons/ai";
 import { AiOutlineDelete, AiFillPlusSquare } from "react-icons/ai";
 import EditTest from "components/EditTest";
 
@@ -25,6 +26,7 @@ export default function Home() {
   const [orderMonth, setOrderMonth] = useState(0);
   const [orderBook, setOrderBook] = useState(0);
   const [orderNumber, setOrderNumber] = useState(0);
+  const [showLove, setShowLove] = useState(0);
   const [filterParameter, setFilterParameter] = useState({
     filter_field: "",
     filter_subField: "",
@@ -32,8 +34,8 @@ export default function Home() {
     filter_month: "",
     filter_book: "",
   });
-  const [showEditPanel, setShowEditPanel] = useState(false)
-  const [dataForEdit, setDataForEdit] = useState({})
+  const [showEditPanel, setShowEditPanel] = useState(false);
+  const [dataForEdit, setDataForEdit] = useState({});
 
   useEffect(() => {
     async function fetchData() {
@@ -44,71 +46,89 @@ export default function Home() {
 
     fetchData();
   }, []);
-  const closePanel=()=>{
-    setDataForEdit({show:false})
-  }
+  const closePanel = () => {
+    setDataForEdit({ show: false });
+  };
 
   const handelOrdering = async () => {
-    const { data } = await axios.get("http://node.readyfortest.ir/admin/test/order", {
-      params: {
-        ...filterParameter,
-        year: orderYear,
-        month: orderMonth,
-        book: orderBook,
-        number: orderNumber,
-      },
-    });
+    const { data } = await axios.get(
+      "http://node.readyfortest.ir/admin/test/order",
+      {
+        params: {
+          ...filterParameter,
+          year: orderYear,
+          month: orderMonth,
+          book: orderBook,
+          number: orderNumber,
+        },
+      }
+    );
 
     setAllTest(data);
   };
 
-  const deleteTest = async (id) =>{
-    const { data } = axios.delete(`http://node.readyfortest.ir/admin/test/delete/${id}`
+  const deleteTest = async (id) => {
+    const { data } = axios.delete(
+      `http://node.readyfortest.ir/admin/test/delete/${id}`
     );
-  }
+  };
 
   return (
     <>
-    <div className="p-4 px-6 bg-gray-200">
-      <div className="flex gap-4 my-2 bg-gray-100 p-3 rounded-lg items-center">
-        <p className="font-bold">فیلتر : </p>
-        <div className="flex flex-1 justify-around">
-          <FieldFilter value={filterParameter} setValue={setFilterParameter} />
-          <SubFieldFilter
-            value={filterParameter}
-            setValue={setFilterParameter}
-          />
-          <YearFilter value={filterParameter} setValue={setFilterParameter} />
-          <MonthFilter value={filterParameter} setValue={setFilterParameter} />
-          <BookFilter value={filterParameter} setValue={setFilterParameter} />
-          <ImageFilter value={filterParameter} setValue={setFilterParameter} />
-        </div>
-      </div>
+      <div className=" py-2 flex justify-center text-sm font-bold bg-gray-200">
+        {showLove >7 ? <p onClick={()=>setShowLove(0)} className="select-none animate-bounce">سارا دوستت دارم :)</p>
+        :
+        <AiFillHeart onClick={()=>{
+          setShowLove(e=>e+1)
+        }} className="h-6 w-6 text-gray-400 hover:text-red-500  active:scale-90 cursor-pointer duration-95 transition-all " />
 
-      <div className="flex gap-4 my-4 bg-gray-100 p-3 rounded-lg items-center">
-        <p className="font-bold">مرتب سازی : </p>
-        <div className="flex flex-1 gap-4 items-center">
-          <YearOrdering setValue={setOrderYear} />
-          <MonthOrdering setValue={setOrderMonth} />
-          <NumberOrdering setValue={setOrderNumber} />
-          <BookOrdering setValue={setOrderBook} />
-          <button
-            onClick={handelOrdering}
-            className=" mr-6 px-8 py-1 active:scale-95  bg-[#4299e1] text-white font-bold  border rounded-lg"
-          >
-            اعمال
-          </button>
-          <div className="mr-auto font-black mx-4 text-xl">
-            {allTest.length}
+      }
+      </div>
+      <div className="select-none pt-0 p-4 px-6 bg-gray-200">
+        <div className="flex gap-4 mb-2 bg-gray-100 p-3 rounded-lg items-center">
+          <p className="font-bold">فیلتر : </p>
+          <div className="flex flex-wrap flex-1 justify-around">
+            <FieldFilter
+              value={filterParameter}
+              setValue={setFilterParameter}
+            />
+            <SubFieldFilter
+              value={filterParameter}
+              setValue={setFilterParameter}
+            />
+            <YearFilter value={filterParameter} setValue={setFilterParameter} />
+            <MonthFilter
+              value={filterParameter}
+              setValue={setFilterParameter}
+            />
+            <BookFilter value={filterParameter} setValue={setFilterParameter} />
+            <ImageFilter
+              value={filterParameter}
+              setValue={setFilterParameter}
+            />
           </div>
         </div>
-      </div>
 
-      {allTest.map(
-        (
-          data,
-          index
-        ) => (
+        <div className="flex gap-4 my-4 bg-gray-100 p-3 rounded-lg items-center">
+          <p className="font-bold">مرتب سازی : </p>
+          <div className="flex flex-wrap flex-1 gap-4 items-center">
+            <YearOrdering setValue={setOrderYear} />
+            <MonthOrdering setValue={setOrderMonth} />
+            <NumberOrdering setValue={setOrderNumber} />
+            <BookOrdering setValue={setOrderBook} />
+            <button
+              onClick={handelOrdering}
+              className=" mr-6 px-8 py-1 active:scale-95  bg-[#4299e1] text-white font-bold  border rounded-lg"
+            >
+              اعمال
+            </button>
+            <div className="mr-auto font-black mx-4 text-xl">
+              {allTest.length}
+            </div>
+          </div>
+        </div>
+
+        {allTest.map((data, index) => (
           <>
             <div
               key={data._id}
@@ -119,10 +139,22 @@ export default function Home() {
                   <p className="ml-2">{index + 1}-</p>
                   <p className="">{data.question}</p>
                 </div>
-                <OptionTest text={data.option1} isAnswer={data.correctAnswer == 1} />
-                <OptionTest text={data.option2} isAnswer={data.correctAnswer == 2} />
-                <OptionTest text={data.option3} isAnswer={data.correctAnswer == 3} />
-                <OptionTest text={data.option4} isAnswer={data.correctAnswer == 4} />
+                <OptionTest
+                  text={data.option1}
+                  isAnswer={data.correctAnswer == 1}
+                />
+                <OptionTest
+                  text={data.option2}
+                  isAnswer={data.correctAnswer == 2}
+                />
+                <OptionTest
+                  text={data.option3}
+                  isAnswer={data.correctAnswer == 3}
+                />
+                <OptionTest
+                  text={data.option4}
+                  isAnswer={data.correctAnswer == 4}
+                />
                 <div className="flex mt-5 justify-around">
                   <p className="bg-gray-200 p-2 rounded-lg shadow-sm text-center">
                     {" "}
@@ -160,20 +192,29 @@ export default function Home() {
                 </p>
               </div>
               <div className="flex pr-4 items-center gap-8">
-                <FiEdit onClick={()=>{
-                  setDataForEdit({show:true,data})}} size={20} className="cursor-pointer hover:scale-95 active:scale-125 duration-200" />
-                <AiOutlineDelete onClick={()=>deleteTest(data._id)} size={20} className="cursor-pointer  hover:scale-95 active:scale-125 duration-200" />
+                <FiEdit
+                  onClick={() => {
+                    setDataForEdit({ show: true, data });
+                  }}
+                  size={20}
+                  className="cursor-pointer hover:scale-95 active:scale-125 duration-200"
+                />
+                <AiOutlineDelete
+                  onClick={() => deleteTest(data._id)}
+                  size={20}
+                  className="cursor-pointer  hover:scale-95 active:scale-125 duration-200"
+                />
               </div>
               {/* <div className="absolute -bottom-4 left-0 right-0 flex justify-center">
                 <AiFillPlusSquare size={30} className="text-gray-500 cursor-pointer  hover:scale-95 active:scale-125 duration-200" />
               </div> */}
-              
             </div>
           </>
-        )
+        ))}
+      </div>
+      {dataForEdit?.show && (
+        <EditTest data={dataForEdit.data} closePanel={closePanel} />
       )}
-    </div>
-    {dataForEdit?.show && <EditTest data={dataForEdit.data} closePanel={closePanel} />} 
     </>
   );
 }
